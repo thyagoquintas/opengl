5114047d50f15157611b4ef7c22a8992ab6ba5ab
Thyago Quintas <thyagoquintas@hotmail.com>
2022-08-29 11:13:49 -0300

    Aula 02

--- Origem.cpp
+++ Origem.cpp
@@ -2,6 +2,74 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
+GLuint VAO, VBO, programa;
+
+
+//Vertex Shader
+static const char* vShader = "                                \n\
+#version 330                                                  \n\
+                                                              \n\
+layout(location=0) in vec2 pos;                               \n\
+                                                              \n\
+                                                              \n\
+void main(){                                                  \n\
+	gl_Position = vec4(pos.x, pos.y, 1.0, 1.0);               \n\
+}";
+
+static const char* fShader = "                                \n\
+#version 330                                                  \n\
+                                                              \n\
+out vec4 color;                                               \n\
+                                                              \n\
+                                                              \n\
+void main(){                                                  \n\
+	color = vec4(1.0, 0.0, 0.0, 1.0);                         \n\
+}";
+
+
+
+void CriaTriangulos() {
+	GLfloat vertices[] = {
+		//x , y		
+		0.0f, 1.0f,
+		-1.0f, -1.0f,
+		1.0f, -1.0f
+	};
+
+	glGenVertexArrays(1, &VAO); //Cria o VAO
+	glBindVertexArray(VAO); //Coloca o VAO em contexto
+	glGenBuffers(1, &VBO); //Cria o VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); //Coloca o VBO em contexto
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //Explica o valor do Array
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0); //Explica os valores de x e y
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0); //remover do contexto o VBO
+	glBindVertexArray(0); //remover do contexto o VAO
+}
+
+
+void CompilaShader() {
+	programa = glCreateProgram(); //Cria um programa
+	GLuint _vShader = glCreateShader(GL_VERTEX_SHADER); //Cria um shader
+	GLuint _fShader = glCreateShader(GL_FRAGMENT_SHADER); //Cria um shader
+
+	//Gambiarra para converter Char em GLChar
+	const GLchar* vCode[1];
+	const GLchar* fCode[1];
+	vCode[0] = vShader; //C�digo do vShader
+	fCode[0] = fShader; //C�digo do fShader
+
+	glShaderSource(_vShader, 1, vCode, NULL); //associa o shader ao codigo
+	glCompileShader(_vShader); //Compila o shader
+	glAttachShader(programa, _vShader); //Adiciona o shader ao programa
+
+	glShaderSource(_fShader, 1, fCode, NULL); //associa o shader ao codigo
+	glCompileShader(_fShader); //Compila o shader
+	glAttachShader(programa, _fShader); //Adiciona o shader ao programa
+
+	glLinkProgram(programa); //Adiciona o programa
+
+}
 
 int main() {
 	if (!glfwInit()) {
@@ -30,6 +98,8 @@ int main() {
 
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
+	CriaTriangulos();
+	CompilaShader();
 
 	while (!glfwWindowShouldClose(mainWindow)) {
 
@@ -38,6 +108,13 @@ int main() {
 
 		glClearColor(1.0f, 0.75f, 0.79f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Desenha o triangulo
+		glUseProgram(programa);
+			glBindVertexArray(VAO);
+				glDrawArrays(GL_TRIANGLES, 0, 3);
+			glBindVertexArray(0);
+		glUseProgram(0);
 
 		glfwSwapBuffers(mainWindow);
 	}
